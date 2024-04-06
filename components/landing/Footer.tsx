6d1097bcc92c0b830a952592df63439ef83df69b
Eva Raymond <evathecoderr@outlook.com>
2024-04-06 19:17:01 +0100

    enhancement

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -5,7 +5,7 @@ import LogoSVG from '@/public/logo.svg'
 export function Footer() {
   return (
     <footer className="px-1">
-      <div className="relative mx-auto flex h-80 flex-col justify-between overflow-hidden rounded-4xl bg-primary-depth pt-0 text-primary-foreground dark:bg-card sm:h-96 sm:w-11/12 lg:h-[26rem]">
+      <div className="relative mx-auto flex h-80 flex-col justify-between overflow-hidden rounded-4xl bg-primary-depth/90 pt-0 text-primary-foreground dark:bg-card sm:h-96 sm:w-11/12 lg:h-[26rem]">
         <div className="flex border-b-2 border-border/70 dark:border-card dark:bg-primary max-md:flex-col-reverse">
           <div className="group flex w-full flex-1 gap-12 overflow-hidden whitespace-nowrap border-border/70 py-2 text-lg capitalize max-md:border-t-2 sm:text-2xl md:border-r-2 md:py-4">
             <p className="flex animate-footer-marquee items-center gap-12 group-hover:paused">
@@ -42,7 +42,7 @@ export function Footer() {
           </a>
         </div>
         <div className="select-none">
-          <p className="font-display text-[min(35vw,300px)] -tracking-widest opacity-85 dark:text-card-foreground md:absolute md:-bottom-1/4">
+          <p className="font-display text-[min(37vw,300px)] -tracking-widest opacity-85 dark:text-card-foreground md:absolute md:-bottom-1/4">
             Lingo
           </p>
           <div className="relative -right-3 top-12 ml-auto w-1/3 drop-shadow-2xl saturate-[0.7] dark:hue-rotate-[50deg] max-md:hidden">
--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,13 +1,16 @@
-import { Earth, Globe } from 'lucide-react'
+import { Globe } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+
+import LangSVG from '@/public/img/lang.svg'
+import VoiceSVG from '@/public/img/voice.svg'
 
 export function Hero() {
   return (
-    <section className="space-y-8 px-4 py-20">
-      <h1 className="flex w-full flex-col items-center justify-center text-balance text-center font-display text-3xl font-bold capitalize tracking-tight max-md:!leading-snug md:gap-4 md:text-5xl">
+    <section className="relative overflow-hidden px-4 py-20">
+      <h1 className="flex w-full flex-col items-center justify-center gap-2 text-balance text-center font-display text-3xl font-bold capitalize leading-normal tracking-tighter sm:leading-snug md:gap-4 md:text-5xl">
         <span>
           Master any{' '}
-          <span className="rounded-xs bg-gradient-to-r from-highlight/50 to-transparent pb-[0.125em] pl-[0.125em]">
+          <span className="rounded-full border border-highlight/25 bg-highlight/50 px-[0.35em] py-[0.125em]">
             language.
           </span>
         </span>
@@ -25,13 +28,23 @@ export function Hero() {
           the go.
         </span>
       </h1>
-      <div className="mx-auto flex max-w-80 flex-col gap-3">
+      <div className="mx-auto my-16 flex max-w-80 flex-col gap-3">
         <Button variant="primary" size="lg">
-          Get Started
+          <span className="truncate">Get started</span>
         </Button>
         <Button size="lg" className="text-secondary">
           <span className="truncate">I already have an account</span>
         </Button>
+      </div>
+      <div className="absolute -left-[2%] top-[10%] -z-1 sm:left-[10%]">
+        <div className="size-20 -rotate-12 rounded-lg bg-gradient-to-br from-highlight/50  to-transparent p-2 text-background md:size-24 lg:size-32">
+          <LangSVG />
+        </div>
+      </div>
+      <div className="absolute -right-[2%] top-1/3 -z-1 sm:right-[10%]">
+        <div className="size-20 rotate-12 rounded-lg bg-gradient-to-bl from-secondary/30  to-transparent p-2 text-background md:size-24 lg:size-32">
+          <VoiceSVG />
+        </div>
       </div>
     </section>
   )
