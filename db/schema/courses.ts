50a844a2ff1c42981c678d2358747fd834859793
Eva Raymond <evathecoderr@outlook.com>
2024-05-18 23:58:58 +0100

    userprogress schema

--- db/schema/courses.ts
+++ db/schema/courses.ts
@@ -1,4 +1,7 @@
+import { relations } from 'drizzle-orm'
 import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core'
+
+import { userProgress } from '@/db/schema/userProgress'
 
 export const courses = pgTable('courses', {
   id: serial('id').primaryKey(),
@@ -6,4 +9,8 @@ export const courses = pgTable('courses', {
   altCode: varchar('alt_code', { length: 2 }).notNull(),
 })
 
-export type Course = typeof courses.$inferSelect
+export const coursesRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}))
+
+export type CourseType = typeof courses.$inferSelect
---
+++ db/schema/userProgress.ts
@@ -0,0 +1,23 @@
+import { relations } from 'drizzle-orm'
+import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core'
+
+import { courses } from '@/db/schema/courses'
+
+export const userProgress = pgTable('user_progress', {
+  id: serial('id').primaryKey(),
+  userName: text('user_name').notNull().default('User'),
+  userImgSrc: text('user_img_src').notNull().default('/mascot.svg'),
+  activeCourseId: integer('active_course_id').references(() => courses.id, { onDelete: 'cascade' }),
+  hearts: integer('hearts').notNull().default(5),
+  points: integer('points').notNull().default(0),
+  gems: integer('gems').notNull().default(0),
+})
+
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}))
+
+export type UserProgressType = typeof userProgress.$inferSelect
